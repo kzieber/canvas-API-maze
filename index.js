718dfd702acb5ffd7eb7d57387e9d11e566cfb22
Kevin Zieber <zieber.kevinr@gmail.com>
2021-04-14 09:53:39 -0700

    generating random shapes with gravity inside canvas now

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ const {
   Mouse,
 } = Matter;
 
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
+    width,
+    height,
   },
 });
 
@@ -47,4 +50,17 @@ const walls = [
 
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+//Random Shapes
+for (let i = 0; i < 20; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 35)
+    );
+  }
+}
