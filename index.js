a46af2d60d0fb3a9067f85b6fe8b22555caf0708
Kevin Zieber <zieber.kevinr@gmail.com>
2021-04-15 09:06:52 -0700

    removed random shapes, refactored walls of canvas

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const {
-  Engine,
-  Render,
-  Runner,
-  World,
-  Bodies,
-  MouseConstraint,
-  Mouse,
-} = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -17,7 +9,7 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    wireframes: false,
+    wireframes: true,
     width,
     height,
   },
@@ -26,42 +18,20 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas),
-  })
-);
-
 //Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, {
+  Bodies.rectangle(width / 2, 0, width, 40, {
     isStatic: true,
   }),
-  Bodies.rectangle(400, 600, 800, 40, {
+  Bodies.rectangle(width / 2, height, width, 40, {
     isStatic: true,
   }),
-  Bodies.rectangle(0, 300, 40, 600, {
+  Bodies.rectangle(0, height / 2, 40, height, {
     isStatic: true,
   }),
-  Bodies.rectangle(800, 300, 40, 600, {
+  Bodies.rectangle(width, height / 2, 40, height, {
     isStatic: true,
   }),
 ];
 
 World.add(world, walls);
-
-//Random Shapes
-for (let i = 0; i < 30; i++) {
-  if (Math.random() > 0.5) {
-    World.add(
-      world,
-      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-    );
-  } else {
-    World.add(
-      world,
-      Bodies.circle(Math.random() * width, Math.random() * height, 35)
-    );
-  }
-}
